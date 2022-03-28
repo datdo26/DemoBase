f9f4e52d0a3b1e3bf074ec3e5743dcd6252f3b9e
datdo26 <102008912+datdo26@users.noreply.github.com>
2022-03-28 17:17:20 +0700

    new_commit

--- App.tsx
+++ App.tsx
@@ -1,9 +1,25 @@
-import {StyleSheet, Text, View} from 'react-native';
+// import {StyleSheet, Text, View} from 'react-native';
+// import React from 'react';
+// import RootStack from './src/nav/RootStack';
+
+// const App = () => {
+//   return <RootStack />;
+// };
+
+// export default App;
+
+// const styles = StyleSheet.create({});
+
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import RootStack from './src/nav/RootStack';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const App = () => {
-  return <RootStack />;
+  return (
+    <SafeAreaView>
+      <Icon name="comments" size={30} color="#900" />
+    </SafeAreaView>
+  );
 };
 
 export default App;
